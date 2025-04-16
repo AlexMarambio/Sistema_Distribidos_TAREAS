8040b1272743cdbd81856efa3273a872fd076c33
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2025-04-16 12:16:52 -0400

    Update wazeScraper.py

--- scraper/wazeScraper.py
+++ scraper/wazeScraper.py
@@ -2,20 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-# Iniciar navegador con configuración por defecto (visualmente)
+# Iniciar navegador
 driver = webdriver.Chrome()
 
 try:
     driver.get("https://www.waze.com/es-419/live-map/")
 
     # Espera fija (puedes reemplazarla con espera dinámica luego)
-    time.sleep(5)
+    time.sleep(50)
 
     # Buscar elementos por clase
-    elementos = driver.find_elements(By.CLASS_NAME, "wm-rgeocoding")
+    elementos = driver.find_elements(By.CLASS_NAME, "wm-map")
 
     print(f"Se encontraron {len(elementos)} elementos:")
     for i, el in enumerate(elementos, 1):
+        try:
+            title = el.find_elements(By.CLASS_NAME, "wm-alert-details__title")
+            print("Tipo alerta:", title)
+        except:
+            print("notitle")
+
         print(f"{i}. {el.text}")
 
 finally:
