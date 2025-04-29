27ea99895b0e3bc7d1c649a3169fe91d96d657a2
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2025-04-29 19:06:06 -0400

    Script bash
    corre venv y scraper de forma automática

---
+++ scraper/run-waze-scraper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source venv311/bin/activate
+
+# Run the Python script
+python wazeScraper.py
+
+# Optional: Deactivate the virtual environment when done
+deactivate
