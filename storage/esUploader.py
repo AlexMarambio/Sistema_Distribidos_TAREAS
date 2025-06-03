4de799e52d68257b36e1df41ab214f2237fb7025
joaquin silva <joaquinignaciosilvasanchez@gmail.com>
2025-06-03 11:29:03 -0400

    intento de apache pig y hadoop y  migrado el mongo a elastic

---
+++ storage/esUploader.py
@@ -0,0 +1,26 @@
+from elasticsearch import Elasticsearch
+import os
+import uuid
+
+# URL del host de Elasticsearch
+ES_HOST = os.getenv("ELASTICSEARCH_HOST", "http://localhost:9200")
+
+# Conexión al cliente de Elasticsearch
+es = Elasticsearch(ES_HOST)
+
+INDEX_NAME = "waze-alerts"
+
+# Función para insertar alertas
+def insert_alerts(alert_list):
+    if alert_list:
+        for alert in alert_list:
+            alert_id = str(uuid.uuid4())
+            es.index(index=INDEX_NAME, id=alert_id, document=alert)
+        print(f"Se insertaron {len(alert_list)} alertas en Elasticsearch.")
+    else:
+        print("No hay alertas para insertar.")
+
+# Función para obtener alertas
+def get_alerts(limit=100):
+    response = es.search(index=INDEX_NAME, size=limit, query={"match_all": {}})
+    return [hit["_source"] for hit in response["hits"]["hits"]]
