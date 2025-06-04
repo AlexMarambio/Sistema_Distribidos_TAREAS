c201689ba766ae56d0a5a14396d109cabcb08de6
Alex Marambio <65684793+AlexMarambio@users.noreply.github.com>
2025-06-03 23:39:30 -0400

    Comment delete

--- storage/mongoUploader.py
+++ storage/mongoUploader.py
@@ -1,15 +1,12 @@
 from pymongo import MongoClient
 import os
 
-# URI de MongoDB Atlas (configurar bien)
 MONGO_URI = os.getenv("MONGO_URI")
 
-# Conexión a MongoDB
 client = MongoClient(MONGO_URI)
 db = client["waze-alerts"]
 alerts_collection = db["alerts"]
 
-# Función para insertar alertas
 def insert_alerts(alert_list):
     if alert_list:
         alerts_collection.insert_many(alert_list)
@@ -17,6 +14,5 @@ def insert_alerts(alert_list):
     else:
         print("No hay alertas para insertar.")
 
-# Función para obtener alertas
 def get_alerts(limit=100):
     return list(alerts_collection.find().limit(limit))
